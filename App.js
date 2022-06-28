9aa3d203ac04be99a4e946ee11c4ee2e704ccbc6
Aakashtyagi123 <aakashtyagi1723@gmail.com>
2022-06-28 22:05:52 +0530

    last branch

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {View,Text} from "react-native"
 function App() {
   return (
-    <View><Text>Code is to short.</Text></View>
+    <View><Text>Code is to shortedddd.</Text></View>
   )
 }
 
