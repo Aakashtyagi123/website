a2f8157272b65efc8f9d81fb2024b6b383efdcbb
Aakashtyagi123 <83485959+Aakashtyagi123@users.noreply.github.com>
2022-06-28 22:00:54 +0530

    some imp

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {View,Text} from "react-native"
 function App() {
   return (
-    <View><Text>Aakash tyagi and my father name is naresh tyagi</Text></View>
+    <View><Text>Aakash tyagi and my father name is naresh tyagi and my mother name is naresh tyagi..</Text></View>
   )
 }
 
